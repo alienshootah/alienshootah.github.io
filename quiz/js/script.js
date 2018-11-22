37f08b0362f72589a14d303843a507868f4f77de
alienshooter <36866487+alienshootah@users.noreply.github.com>
2018-11-22 22:24:01 +0200

    commit

---
+++ quiz/js/script.js
@@ -0,0 +1,34 @@
+        // в этой задаче неважно, как именно прятать элемент
+        // например через style.display:
+        document.getElementById('hider').onclick = function() {
+            document.getElementById('text').style.opacity = '0';
+            document.getElementById('text').style.visibility = 'hidden';
+        }
+        document.getElementById('hider2').onclick = function() {
+            document.getElementById('text2').style.opacity = '0';
+            document.getElementById('text2').style.visibility = 'hidden';
+        }
+        document.getElementById('hider3').onclick = function() {
+            document.getElementById('text2').style.opacity = '0';
+            document.getElementById('text2').style.visibility = 'hidden';
+        }
+        document.getElementById('hider4').onclick = function() {
+            document.getElementById('text2').style.opacity = '0';
+            document.getElementById('text2').style.visibility = 'hidden';
+        }
+        document.getElementById('hider5').onclick = function() {
+            document.getElementById('text3').style.opacity = '0';
+            document.getElementById('text3').style.visibility = 'hidden';
+        }
+        document.getElementById('hider6').onclick = function() {
+            document.getElementById('text3').style.opacity = '0';
+            document.getElementById('text3').style.visibility = 'hidden';
+        }
+        document.getElementById('hider7').onclick = function() {
+            document.getElementById('text3').style.opacity = '0';
+            document.getElementById('text3').style.visibility = 'hidden';
+        }
+        document.getElementById('hider8').onclick = function() {
+            document.getElementById('text4').style.opacity = '0';
+            document.getElementById('text4').style.visibility = 'hidden';
+        }
