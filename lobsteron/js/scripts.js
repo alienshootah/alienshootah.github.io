fdfcb1d4d4f112ccdaf6f35f2afb9d526de1bef0
alienshooter <36866487+alienshootah@users.noreply.github.com>
2018-11-30 20:43:58 +0200

    commit

--- lobsteron/js/scripts.js
+++ lobsteron/js/scripts.js
@@ -1,13 +1,17 @@
+
+// Скрипт крестика в меню (был)
 function myFunction(x) {
     x.classList.toggle("change");
 }
+
+// Скрипт модального окна на главной
 // A $( document ).ready() block.
 $( document ).ready(function() {
     // load the overlay
     
-     
-   /* 
     $('#myModal').modal({show:true});
+   /* 
+    
    if (document.cookie.indexOf('visited=true') == -1){
       здесь было тру
       
@@ -17,5 +21,13 @@ $( document ).ready(function() {
   
     } */
   }); 
-  
-  
+
+  // Скрипт скрытия подсказки
+  function myFunction(y) {
+    var y = document.getElementById("myDIV");
+    if (y.style.display === "none") {
+        y.style.display = "block";
+    } else {
+        y.style.display = "none";
+    }
+}
