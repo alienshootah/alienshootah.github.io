6a571007502f27dc756f83bc59775d57476afa33
alienshooter <36866487+alienshootah@users.noreply.github.com>
2018-11-30 17:36:18 +0200

    commit

--- lobsteron/js/scripts.js
+++ lobsteron/js/scripts.js
@@ -1,3 +1,21 @@
 function myFunction(x) {
     x.classList.toggle("change");
 }
+// A $( document ).ready() block.
+$( document ).ready(function() {
+    // load the overlay
+    
+     
+   /* 
+    $('#myModal').modal({show:true});
+   if (document.cookie.indexOf('visited=true') == -1){
+      здесь было тру
+      
+      var year = 1000*60*60*24*365;
+      var expires = new Date((new Date()).valueOf() + year);
+      document.cookie = "visited=true;expires=" + expires.toUTCString();
+  
+    } */
+  }); 
+  
+  
