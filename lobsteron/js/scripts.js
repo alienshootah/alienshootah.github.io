875855450d406b51230c9ef31c8206fff77f1513
alienshooter <36866487+alienshootah@users.noreply.github.com>
2018-12-01 16:14:34 +0200

    commit

--- lobsteron/js/scripts.js
+++ lobsteron/js/scripts.js
@@ -31,3 +31,33 @@ $( document ).ready(function() {
         y.style.display = "none";
     }
 }
+
+ /* Скрипт адаптивного слайдера с плавным переходом за 1 секунду, бесконечный */
+           
+ var btn_prev = document.querySelector('#gallery .buttons .prev');
+ var btn_next = document.querySelector('#gallery .buttons .next');
+
+ var images = document.querySelectorAll('#gallery .photos img');
+ var i = 0;
+
+ btn_prev.onclick = function() {
+ images[i].className = '';
+ i = i - 1; /* i++ */
+ 
+ if(i < 0) {
+ i = images.length - 1;
+ }
+ 
+ images[i].className = 'showed';
+ }
+
+ btn_next.onclick = function() {
+ images[i].className = '';
+ i = i + 1; /* i++ */
+ 
+ if(i >= images.length) {
+ i = 0;
+ }
+ 
+ images[i].className = 'showed';
+ }
