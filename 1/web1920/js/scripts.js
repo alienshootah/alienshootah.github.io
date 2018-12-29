ff291eb7e4ee16897672699f326a1421e9c0379a
alienshooter <36866487+alienshootah@users.noreply.github.com>
2018-12-30 01:05:13 +0200

    commit

--- 1/web1920/js/scripts.js
+++ 1/web1920/js/scripts.js
@@ -1,3 +1,8 @@
+// Иконка меню анимация на 768 пикселей
+function myFun(x) {
+  x.classList.toggle("change");
+}
+
 $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip(); 
   });
