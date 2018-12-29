e92eb0116b078a9331b85b9b0b525c773294dcc7
alienshooter <36866487+alienshootah@users.noreply.github.com>
2018-12-29 14:01:21 +0200

    commit

--- 1/org/js/sliders.js
+++ 1/org/js/sliders.js
@@ -1,52 +1,37 @@
-
+// Иконка меню анимация на 768 пикселей
+function myFun(x) {
+  x.classList.toggle("change");
+}
 
 // Слайдеры
 $('.multiple-items').slick({
     infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
+    slidesToShow: 3,
+    slidesToScroll: 3,
     speed: 300,
     adaptiveHeight: true,
-    dots: true,
-    centerMode: true,
-    responsive: [
-      {
-        breakpoint: 1260,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          centerMode: false,
-          arrows: false,
-          dots: false
-        }
-        
-      }
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
-    ]
-  });
-  $('.single-item').slick({
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    speed: 300,
-    dots: true,
-    adaptiveHeight: true,
+    dots: false,
     arrows: true,
     responsive: [
       {
-        breakpoint: 1260,
+        breakpoint: 1340,
+        settings: {
+        arrows: false,
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        }
+      },
+      {
+        breakpoint: 770,
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
-          centerMode: false,
-          arrows: false,
-          dots: false
+          arrows: false
         }
       }
       // You can unslick at a given breakpoint now by adding:
       // settings: "unslick"
       // instead of a settings object
     ]
+   
   });
