15e8c59e0c4f0fee6caac8bea8382e35270dd8ba
alienshooter <36866487+alienshootah@users.noreply.github.com>
2019-01-11 21:12:27 +0200

    commit

---
+++ 1/bazar/js/scripts.js
@@ -0,0 +1,41 @@
+
+// User menu
+function userNav() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+}
+
+// Custom Dropdown
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myDrop() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+function myDrop2() {
+    document.getElementById("myDropdown2").classList.toggle("show");
+}
+function myDrop3() {
+    document.getElementById("myDropdown3").classList.toggle("show");
+}
+function myDrop4() {
+    document.getElementById("myDropdown4").classList.toggle("show");
+}
+  
+  
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+    var openDropdown = dropdowns[i];
+    if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+    }
+    }
+}
+}
