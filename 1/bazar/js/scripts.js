a020cd7ae319c78780063920199eb2a6729a03db
alienshooter <36866487+alienshootah@users.noreply.github.com>
2019-01-12 16:03:23 +0200

    commit

--- 1/bazar/js/scripts.js
+++ 1/bazar/js/scripts.js
@@ -142,3 +142,69 @@ $('.single-item').slick({
     slidesToShow: 1,
     adaptiveHeight: true
 });
+
+// Timer
+function updater(d, h, m, s) {
+  // День сброса - 27 сентября 2015 года (и далее каждые три дня)
+  var baseTime = new Date(2015, 8, 27);
+  // Период сброса — 3 дня
+  var period = 24*60*60*1000;
+
+  function update() {
+  var cur = new Date();
+  // сколько осталось миллисекунд
+  var diff = period - (cur - baseTime) % period;
+  // сколько миллисекунд до конца секунды
+  var millis = diff % 1000;
+  diff = Math.floor(diff/1000);
+  // сколько секунд до конца минуты
+  var sec = diff % 60;
+  if(sec < 10) sec = "0"+sec;
+  diff = Math.floor(diff/60);
+  // сколько минут до конца часа
+  var min = diff % 60;
+  if(min < 10) min = "0"+min;
+  diff = Math.floor(diff/60);
+  // сколько часов до конца дня
+  var hours = diff % 24;
+  if(hours < 10) hours = "0"+hours;
+  // сколько часов до конца дня
+  var days = diff % 30;
+  if(days < 10) days = "0"+days;
+  
+  d.innerHTML = days;
+  h.innerHTML = hours;
+  m.innerHTML = min;
+  s.innerHTML = sec;
+  
+  // следующий раз вызываем себя, когда закончится текущая секунда
+  setTimeout(update, millis);
+  }
+  setTimeout(update, 0);
+}
+
+updater(document.getElementById("days"),
+ document.getElementById("hours"), document.getElementById("minutes"),
+ document.getElementById("seconds"));
+
+// Modal Boxes
+// Get the modal
+var modal = document.getElementById('myModal');
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
