e59c0d5f870faacbab5394414b51453933b28847
alienshooter <36866487+alienshootah@users.noreply.github.com>
2019-01-12 21:26:19 +0200

    commit

--- 1/bazar/js/scripts.js
+++ 1/bazar/js/scripts.js
@@ -97,7 +97,31 @@ $('.slider-nav').slick({
     dots: false,
     arrows: true,
     centerMode: true,
-    focusOnSelect: true
+    focusOnSelect: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 2,
+          infinite: true,
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+    }
+  ]
 });
 $('.responsive').slick({
     dots: false,
