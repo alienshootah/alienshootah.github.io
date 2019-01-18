5ff2301c60d65665fc2aa33719bcf18129e21d92
alienshooter <36866487+alienshootah@users.noreply.github.com>
2019-01-18 19:15:34 +0200

    commit

--- 1/zabor/js/sliders.js
+++ 1/zabor/js/sliders.js
@@ -16,22 +16,51 @@ $('.one-time').slick({
       {
         breakpoint: 1340,
         settings: {
-        arrows: false,
+        arrows: true,
         slidesToShow: 1,
         slidesToScroll: 1,
         }
       },
       {
-        breakpoint: 610,
+        breakpoint: 620,
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
-          arrows: false
+          arrows: false,
         }
       }
       // You can unslick at a given breakpoint now by adding:
       // settings: "unslick"
       // instead of a settings object
     ]
-   
-  });
+}); 
+$('.multiple-items').slick({
+  infinite: true,
+  slidesToShow: 2,
+  slidesToScroll: 2,
+  speed: 300,
+  adaptiveHeight: true,
+  dots: false,
+  arrows: true,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+      arrows: true,
+      slidesToShow: 2,
+      slidesToScroll: 2,
+      }
+    },
+    {
+      breakpoint: 575,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
