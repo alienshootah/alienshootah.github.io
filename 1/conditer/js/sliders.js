5436e2bead137883b6ac8200096400a08abc70da
alienshooter <36866487+alienshootah@users.noreply.github.com>
2019-02-19 08:38:50 +0200

    commit

--- 1/conditer/js/sliders.js
+++ 1/conditer/js/sliders.js
@@ -66,8 +66,18 @@ toggle between hiding and showing the dropdown content */
 function myFunction() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
+function myFunction2() {
+  document.getElementById("myDropdown2").classList.toggle("show");
+}
+function myFunction3() {
+  document.getElementById("myDropdown3").classList.toggle("show");
+}
+function myFunction4() {
+  document.getElementById("myDropdown4").classList.toggle("show");
+}
 
 // Close the dropdown menu if the user clicks outside of it
+/*
 window.onclick = function(event) {
   if (!event.target.matches('.dropbtn')) {
     var dropdowns = document.getElementsByClassName("dropdown-content");
@@ -80,33 +90,9 @@ window.onclick = function(event) {
     }
   }
 }
+*/
 
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content-down");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
 
-// Дропдауны
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
-function myFunction2() {
-  document.getElementById("myDropdown2").classList.toggle("show");
-}
-function myFunction3() {
-  document.getElementById("myDropdown3").classList.toggle("show");
-}
-function myFunction4() {
-  document.getElementById("myDropdown4").classList.toggle("show");
-}
 
 // Аккордеон
 var acc = document.getElementsByClassName("left-link");
