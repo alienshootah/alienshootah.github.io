623214c535367976a4310028e787bd96e35797cb
alienshooter <36866487+alienshootah@users.noreply.github.com>
2019-03-27 12:21:49 +0200

    commit

---
+++ 2019/03/rock/js/apps.js
@@ -0,0 +1,10 @@
+$('.one-time').slick({
+    dots: false,
+    arrows: false,
+    infinite: true,
+    speed: 900,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    autoplay: false,
+    autoplaySpeed: 5000,
+});
