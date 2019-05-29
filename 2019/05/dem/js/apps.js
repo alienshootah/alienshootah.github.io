7ab971dcddf4232f3f91b63a818d12b34320d64b
alienshooter <36866487+alienshootah@users.noreply.github.com>
2019-05-29 14:34:01 +0300

    commit

--- 2019/05/dem/js/apps.js
+++ 2019/05/dem/js/apps.js
@@ -3,6 +3,12 @@
 function dopDroprown() {
     document.getElementById("dropdown-dop").classList.toggle("show");
 }
+function dopDroprown() {
+    document.getElementById("dropdown-dop").classList.toggle("show");
+}
+function searchDrop() {
+    document.getElementById("search-me").classList.toggle("show");
+}
 
 /* Видео на главной */
 // Изображение в видео запуск ролика
@@ -24,4 +30,5 @@ $(".vedpar-self").each(function () {
 // В видео убирает прозрачность при запуске
 function myVedpar() {
     document.getElementById("main-video").classList.toggle("show");
+    document.getElementById("main-video2").classList.toggle("hide");
 }
