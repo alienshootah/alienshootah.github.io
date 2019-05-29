2ed166f0538f841af9e940dbfff23ddb275b1a15
alienshooter <36866487+alienshootah@users.noreply.github.com>
2019-05-29 10:53:43 +0300

    commit

---
+++ 2019/05/dem/js/apps.js
@@ -0,0 +1,27 @@
+
+/* Дропдауны */
+function dopDroprown() {
+    document.getElementById("dropdown-dop").classList.toggle("show");
+}
+
+/* Видео на главной */
+// Изображение в видео запуск ролика
+$(".vedpar-self").each(function () {
+    var video = $(this).find("video");
+    var plainVideo = video.get(0);/*DOM video object, unwrapped from jQuery*/
+    var playBtn = $(this).find("div");
+
+    playBtn.click(function () {
+        video.get(0).addEventListener('ended',myHandler,false);
+        plainVideo.play();
+        playBtn.css("visibility", "hidden");
+
+        function myHandler(e) {
+            playBtn.css("visibility", "visible");
+        }
+    });
+});
+// В видео убирает прозрачность при запуске
+function myVedpar() {
+    document.getElementById("main-video").classList.toggle("show");
+}
