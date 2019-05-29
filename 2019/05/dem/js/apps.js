6c6bcc1b0b95718c48e41f3313bd94e13aeb54b2
alienshooter <36866487+alienshootah@users.noreply.github.com>
2019-05-29 17:03:56 +0300

    commit

--- 2019/05/dem/js/apps.js
+++ 2019/05/dem/js/apps.js
@@ -17,7 +17,7 @@ window.onclick = function(event) {
       var i;
       for (i = 0; i < dropdowns.length; i++) {
         var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
+        if (openDropdown.classList.toggle('show')) {
           openDropdown.classList.remove('show');
         }
       }
