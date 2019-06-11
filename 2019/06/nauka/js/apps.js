c4f4102007e86bd10229420f051d522d898adc4e
alienshooter <36866487+alienshootah@users.noreply.github.com>
2019-06-11 16:57:04 +0300

    commit

---
+++ 2019/06/nauka/js/apps.js
@@ -0,0 +1,8 @@
+/* Дивы вместо радиокнопок */
+$('.radio-group .radio').click(function(){
+    $(this).parent().find('.radio').removeClass('selected');
+    $(this).addClass('selected');
+    var val = $(this).attr('data-value');
+    //alert(val);
+    $(this).parent().find('input').val(val);
+});
