c1cc11a684c7f8a90394567950586abfbbc9cb9e
alienshooter <36866487+alienshootah@users.noreply.github.com>
2019-06-17 18:55:25 +0300

    commit

--- 2019/06/exams-app/exams-app/nauka/js/apps.js
+++ 2019/06/exams-app/exams-app/nauka/js/apps.js
@@ -11,3 +11,21 @@ $('.radio-group .radio').click(function(){
     //alert(val);
     $(this).parent().find('input').val(val);
 });
+
+/* Чекбоксы */
+$(function(){
+    var checkboxs = $('input[type=checkbox]');
+    
+    checkboxs.each(function(){
+      $(this).wrap('<div class="customCheckbox"></div>');
+      $(this).before('<span>&#10004;</span>');
+    });
+    
+    checkboxs.change(function(){
+      if($(this).is(':checked')){
+       $(this).parent().addClass('customCheckboxChecked');
+      } else {
+       $(this).parent().removeClass('customCheckboxChecked');
+      }
+    });
+  })
