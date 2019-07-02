0e9d85c5b3685acd2630cad5f69e3be85cfcedb8
alienshooter <36866487+alienshootah@users.noreply.github.com>
2019-07-02 09:36:21 +0300

    commit

---
+++ 2019/06/vsgithub/js/apps.js
@@ -0,0 +1,12 @@
+// Слайдер
+$('.middle-center').slick({
+    centerMode: true,
+    centerPadding: '0px',
+    slidesToShow: 3,
+    dots: true,
+    /*
+    prevArrow: $('.awards-pagination_arrow-left'),
+    nextArrow: $('.awards-pagination_arrow-right'),
+    */
+    arrows: false
+});
