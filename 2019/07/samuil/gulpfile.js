520bc06c36c16db46be05736b19dee90e91c8ae0
alienshooter <36866487+alienshootah@users.noreply.github.com>
2019-07-19 02:16:40 +0300

    commit

--- 2019/07/samuil/gulpfile.js
+++ 2019/07/samuil/gulpfile.js
@@ -1,18 +1,21 @@
 /* 
-
-Чтобы скомпилировать scss через gulp, нужно:
-0. Установить node.js, зайти в терминал.
-1. Зайти в папку с проектом
-2. Установить gulp в проект командой:
-
-
+    Чтобы скомпилировать scss через gulp, нужно:
+    0. Установить node.js, зайти в терминал.
+    1. Зайти в папку с проектом
+    2. Установить gulp и модули в проект командами:
+        npm i gulp
+        npm init
+        npm i gulp-sass gulp-sourcemaps gulp-watch --SD
+    3. Написать код ниже:
 */
 
+// Переменные
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const sourcemaps = require('gulp-sourcemaps');
 const watch = require('gulp-watch');
 
+// Компиляция css с scss файла
 gulp.task('sass-compile', function() {
     return gulp.src('./css/**/*.scss')
     .pipe(sourcemaps.init())
@@ -21,6 +24,7 @@ gulp.task('sass-compile', function() {
     .pipe(gulp.dest('./css/'))
 });
 
+// Компиляция css с scss файла в онлайн режиме (работает даже в htmlpad)
 gulp.task('watch', function() {
     gulp.watch('./css/**/*.scss', gulp.series('sass-compile'));
 });
