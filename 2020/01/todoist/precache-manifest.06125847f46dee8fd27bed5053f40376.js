b0863913f7091c98786d16e6729b9ea77ff4d393
alienshootah <shootersmal@gmail.com>
2020-01-31 17:50:44 +0200

    initial commit

---
+++ 2020/01/todoist/precache-manifest.06125847f46dee8fd27bed5053f40376.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81942b8f1c301ac32f3ff3916d3f8721",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c93f497fca89a7cd2457",
+    "url": "/static/css/main.b76bb0c6.chunk.css"
+  },
+  {
+    "revision": "75e75263ae6a0ea0db41",
+    "url": "/static/js/2.31bda61b.chunk.js"
+  },
+  {
+    "revision": "c29216ace69b78af468bfd0f329b39f1",
+    "url": "/static/js/2.31bda61b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c93f497fca89a7cd2457",
+    "url": "/static/js/main.8ed28e75.chunk.js"
+  },
+  {
+    "revision": "433e05bc8e992e226546",
+    "url": "/static/js/runtime-main.29f099e5.js"
+  }
+]);
