7787635fc4ce59bc1b066f69aa6b057a1da320b0
alienshootah <shootersmail@gmail.com>
2021-01-11 07:17:31 +0200

    test react build

---
+++ react/2021/01/bazar/precache-manifest.261f75829a5f778419c957917bcba467.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18ee591e2a84bbefe46e4a0d2a030ad8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cbdcfc12355e4feb77ba",
+    "url": "/static/css/2.1b762aec.chunk.css"
+  },
+  {
+    "revision": "0d860b2084d3f576bca9",
+    "url": "/static/css/main.55955759.chunk.css"
+  },
+  {
+    "revision": "cbdcfc12355e4feb77ba",
+    "url": "/static/js/2.22bb4083.chunk.js"
+  },
+  {
+    "revision": "339bc2301027dc6f62f3170009b3a391",
+    "url": "/static/js/2.22bb4083.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d860b2084d3f576bca9",
+    "url": "/static/js/main.3611906f.chunk.js"
+  },
+  {
+    "revision": "4bae910198572979c526",
+    "url": "/static/js/runtime-main.1cca3602.js"
+  },
+  {
+    "revision": "3352653dedd571bbc490c8be132b38cd",
+    "url": "/static/media/Poppins-Light.3352653d.ttf"
+  },
+  {
+    "revision": "a4e11dda40531debd374e4c8b1dcc7f4",
+    "url": "/static/media/Poppins-Medium.a4e11dda.ttf"
+  },
+  {
+    "revision": "731a28a413d642522667a2de8681ff35",
+    "url": "/static/media/Poppins-Regular.731a28a4.ttf"
+  },
+  {
+    "revision": "e63b93dfac2600782654e2b87910d681",
+    "url": "/static/media/Poppins-SemiBold.e63b93df.ttf"
+  },
+  {
+    "revision": "92f08fa0de3db7570c748bf20149459a",
+    "url": "/static/media/hot-item1.92f08fa0.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
