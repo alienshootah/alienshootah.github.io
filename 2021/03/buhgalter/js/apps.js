6487bcc7d07ef3d3101deec4b64fb50eab97146e
alienshootah <shootersmail@gmail.com>
2021-04-02 11:44:34 +0300

    adaptive 1376px almost

--- 2021/03/buhgalter/js/apps.js
+++ 2021/03/buhgalter/js/apps.js
@@ -16,17 +16,14 @@ $('.promo-list').slick({
   speed: 300,
   slidesToShow: 1,
   adaptiveHeight: true,
-  /*
   responsive: [
       {
-        breakpoint: 768,
+        breakpoint: 1720,
         settings: {
-          arrows: false,
-          dots: false
+          dots: true
         }
       }
     ]
-    */
 });
 $('.diplom-list').slick({
   dots: true,
@@ -70,30 +67,6 @@ function openTab(evt, tabName) {
 
 // Get the element with id="defaultOpen" and click on it
 document.getElementById("defaultOpen").click();
-
-/* Custom range
-const allRanges = document.querySelectorAll(".range-wrap");
-allRanges.forEach(wrap => {
-  const range = wrap.querySelector(".range");
-  const bubble = wrap.querySelector(".bubble");
-
-  range.addEventListener("input", () => {
-    setBubble(range, bubble);
-  });
-  setBubble(range, bubble);
-});
-
-function setBubble(range, bubble) {
-  const val = range.value;
-  const min = range.min ? range.min : 0;
-  const max = range.max ? range.max : 100;
-  const newVal = Number(((val - min) * 100) / (max - min));
-  bubble.innerHTML = val;
-
- 
-  bubble.style.left = `calc(${newVal}% + (${8 - newVal * 0.15}px))`;
-}
-*/
 
 // Filters: Range
 var slider = document.getElementById("online-range");
