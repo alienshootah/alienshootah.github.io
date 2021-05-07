f29eeb23ea8164a4ecf0d14d7d73298c5b29889f
alienshootah <shootersmail@gmail.com>
2021-05-07 13:32:34 +0300

    modal and js

---
+++ 2021/05/dista/js/apps.js
@@ -0,0 +1,52 @@
+
+/* Dropdowns */
+function myDrop1() {
+    document.getElementById("myDrop1").classList.toggle("show");
+}
+function myDrop2() {
+    document.getElementById("myDrop2").classList.toggle("show");
+}
+function myDrop3() {
+    document.getElementById("myDrop3").classList.toggle("show");
+}
+
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.menu-link')) {
+      var dropdowns = document.getElementsByClassName("drop");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+}
+
+/* Modal */
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("modal-close-icon")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
