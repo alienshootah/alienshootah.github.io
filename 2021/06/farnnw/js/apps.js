9c08eb63bc0cfd0dd15fac3cc698a10064ac475e
alienshootah <shootersmail@gmail.com>
2021-06-05 08:24:27 +0300

    project page and fixed header in >768px

--- 2021/06/farnnw/js/apps.js
+++ 2021/06/farnnw/js/apps.js
@@ -9,17 +9,26 @@ mobileBtn.addEventListener("click", function() {
 function myDrop1() {
     document.getElementById("myDrop1").classList.toggle("show");
 }
+function myDrop2() {
+  document.getElementById("myDrop2").classList.toggle("show");
+}
+function myDrop3() {
+  document.getElementById("myDrop3").classList.toggle("show");
+}
+function myDrop4() {
+  document.getElementById("myDrop4").classList.toggle("show");
+}
 
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function(event) {
-    if (!event.target.matches('.menu-link')) {
-      var dropdowns = document.getElementsByClassName("drop");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
+  if (!event.target.matches('.drop-btn')) {
+    var dropdowns = document.getElementsByClassName("drop-body");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
       }
     }
+  }
 }
