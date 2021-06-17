3bfc2a34693bc0ebf150090dd7b66fe38e9348ee
alienshootah <shootersmail@gmail.com>
2021-06-17 06:33:17 +0300

    add catalog page

--- 2021/06/flowers/js/filter.js
+++ 2021/06/flowers/js/filter.js
@@ -4,3 +4,10 @@ const filterBtn = document.querySelector(".cata-filter-btn");
 filterBtn.addEventListener("click", function() {
   document.querySelector(".cata-body").classList.toggle("full");
 });
+
+/* Filter close */
+
+const closeBtn = document.querySelector(".filters-mobile-btn");
+closeBtn.addEventListener("click", function() {
+  document.querySelector(".cata-body").classList.remove("full");
+});
