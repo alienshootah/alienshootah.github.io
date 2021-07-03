257e7e5e2c0ea63c3895249dde5f61990b0db11a
alienshootah <shootersmail@gmail.com>
2021-07-03 15:39:18 +0300

    add tabs js to places page

--- 2021/06/et/js/scripts.js
+++ 2021/06/et/js/scripts.js
@@ -9,3 +9,21 @@ jQuery(document).ready(function($) {
 		$('body').removeClass('no-scroll');
 	});
 });
+
+/* Tabs */
+function openTab(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tabs-btn");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+  
+  // Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
