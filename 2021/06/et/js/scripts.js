acf0b7ebfc62a0ad98169c7acc11d939374a4cff
alienshootah <shootersmail@gmail.com>
2021-07-03 16:54:43 +0300

    add slider to single page

--- 2021/06/et/js/scripts.js
+++ 2021/06/et/js/scripts.js
@@ -10,20 +10,22 @@ jQuery(document).ready(function($) {
 	});
 });
 
-/* Tabs */
-function openTab(evt, cityName) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
+/* Slider */
+$('.single-list').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  dots: false,
+  arrows: true,
+  focusOnSelect: false,
+  responsive: [
+    {
+      breakpoint: 420,
+      settings: {
+        arrows: false,
+		autoplay: true,
+  		autoplaySpeed: 3000,
+      }
     }
-    tablinks = document.getElementsByClassName("tabs-btn");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(cityName).style.display = "block";
-    evt.currentTarget.className += " active";
-  }
-  
-  // Get the element with id="defaultOpen" and click on it
-document.getElementById("defaultOpen").click();
+  ]
+});
+
