4481a9baae433a7d1b70aecbac67f460188bf781
alienshootah <shootersmail@gmail.com>
2021-07-29 15:52:55 +0300

    tech with js

--- 2021/07/whitepaper/js/apps.js
+++ 2021/07/whitepaper/js/apps.js
@@ -7,3 +7,32 @@ mobileBtn.addEventListener("click", function() {
 /* Current year in footer */
 year = document.querySelector('.footer-date');
 year.innerHTML = new Date().getFullYear();
+
+/* Tabs */
+  window.addEventListener("load", function() {
+    // store tabs variable
+    var theTabs = document.querySelectorAll("ul.menutab-list > li");
+
+    function theTabClicks(tabClickEvent) {
+        var clickedTab = tabClickEvent.currentTarget;
+        var tabParent = tabClickEvent.currentTarget.parentNode.parentNode.parentNode;
+        var theTabs = tabParent.querySelectorAll("ul.menutab-list > li");
+        for (var i = 0; i < theTabs.length; i++) {
+            theTabs[i].classList.remove("active");
+        }
+        
+        clickedTab.classList.add("active");
+        tabClickEvent.preventDefault();
+        var contentPanes = tabParent.querySelectorAll(".tabs-content");
+        for (i = 0; i < contentPanes.length; i++) {
+            contentPanes[i].classList.remove("active");
+        }
+        var anchorReference = tabClickEvent.target;
+        var activePaneId = anchorReference.getAttribute("href");
+        var activePane = tabParent.querySelector(activePaneId);
+        activePane.classList.add("active");
+    }
+    for (i = 0; i < theTabs.length; i++) {
+        theTabs[i].addEventListener("click", theTabClicks)
+    }
+});
