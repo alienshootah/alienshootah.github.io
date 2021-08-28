54811050c6c2ea9aa9ce1acd70a05d474de9c6ad
alienshootah <shootersmail@gmail.com>
2021-08-28 09:39:31 +0300

    fish page tabs js ready. urovo project

--- 2021/08/urovo/js/apps.js
+++ 2021/08/urovo/js/apps.js
@@ -137,3 +137,32 @@ window.addEventListener("DOMContentLoaded", function() {
 /* Current year in footer */
 year = document.querySelector('.footer-date');
 year.innerHTML = new Date().getFullYear();
+
+/* Tabs */
+window.addEventListener("load", function() {
+  // store tabs variable
+  var theTabs = document.querySelectorAll("ul.fish-nav > li");
+
+  function theTabClicks(tabClickEvent) {
+      var clickedTab = tabClickEvent.currentTarget;
+      var tabParent = tabClickEvent.currentTarget.parentNode.parentNode.parentNode;
+      var theTabs = tabParent.querySelectorAll("ul.fish-nav > li");
+      for (var i = 0; i < theTabs.length; i++) {
+          theTabs[i].classList.remove("active");
+      }
+      
+      clickedTab.classList.add("active");
+      tabClickEvent.preventDefault();
+      var contentPanes = tabParent.querySelectorAll(".fish-content");
+      for (i = 0; i < contentPanes.length; i++) {
+          contentPanes[i].classList.remove("active");
+      }
+      var anchorReference = tabClickEvent.target;
+      var activePaneId = anchorReference.getAttribute("href");
+      var activePane = tabParent.querySelector(activePaneId);
+      activePane.classList.add("active");
+  }
+  for (i = 0; i < theTabs.length; i++) {
+      theTabs[i].addEventListener("click", theTabClicks)
+  }
+});
