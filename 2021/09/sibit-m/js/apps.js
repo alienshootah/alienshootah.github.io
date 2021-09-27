1c5a6b4c0b84eb9cd41a911948e83c3409d3164f
alienshootah <shootersmail@gmail.com>
2021-09-27 09:43:12 +0300

    modal, current year ready on sibit-m project

---
+++ 2021/09/sibit-m/js/apps.js
@@ -0,0 +1,84 @@
+/* Menu btn */
+const menuBtn = document.querySelector(".header-mobile-btn");
+if(menuBtn) {
+    menuBtn.addEventListener("click", function() {
+      document.querySelector(".header").classList.toggle("show");
+    });
+}
+
+
+
+/* Modals */
+
+let buttons = document.querySelectorAll('.curr-modal');
+let modals = document.querySelectorAll('.modal');
+
+function showModal(id) {
+  let m = document.getElementById(id);
+  m.classList.add('visible');
+}
+
+function hideModals() {
+  modals.forEach(m => {
+    m.classList.remove('visible');
+  });
+}
+
+buttons.forEach(b => {
+  b.addEventListener('click', event => {
+    hideModals();
+    showModal(b.dataset.modal);
+  });
+});
+
+
+/**/
+modals.forEach(m => {
+  let x = m.querySelector('button.close');
+  if (x !== 'modal-body') {
+    x.addEventListener('click', hideModals);
+  }
+});
+
+/* Telephone mask */
+
+window.addEventListener("DOMContentLoaded", function() {
+    [].forEach.call( document.querySelectorAll('.tel'), function(input) {
+    var keyCode;
+    function mask(event) {
+        event.keyCode && (keyCode = event.keyCode);
+        var pos = this.selectionStart;
+        if (pos < 3) event.preventDefault();
+        var matrix = "+7 (___) ___ ____",
+            i = 0,
+            def = matrix.replace(/\D/g, ""),
+            val = this.value.replace(/\D/g, ""),
+            new_value = matrix.replace(/[_\d]/g, function(a) {
+                return i < val.length ? val.charAt(i++) || def.charAt(i) : a
+            });
+        i = new_value.indexOf("_");
+        if (i != -1) {
+            i < 5 && (i = 3);
+            new_value = new_value.slice(0, i)
+        }
+        var reg = matrix.substr(0, this.value.length).replace(/_+/g,
+            function(a) {
+                return "\\d{1," + a.length + "}"
+            }).replace(/[+()]/g, "\\$&");
+        reg = new RegExp("^" + reg + "$");
+        if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) this.value = new_value;
+        if (event.type == "blur" && this.value.length < 5)  this.value = ""
+    }
+  
+    input.addEventListener("input", mask, false);
+    input.addEventListener("focus", mask, false);
+    input.addEventListener("blur", mask, false);
+    input.addEventListener("keydown", mask, false)
+  
+  });
+  
+});
+
+/* Current year in footer */
+year = document.querySelector('.footer-date');
+year.innerHTML = new Date().getFullYear();
