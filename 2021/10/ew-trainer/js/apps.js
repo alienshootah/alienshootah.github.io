c072432234ddfe218244f8c39f8f4baa2cf13164
alienshootah <shootersmail@gmail.com>
2021-10-04 16:01:48 +0300

    ew-trainer project. workout page css

--- 2021/10/ew-trainer/js/apps.js
+++ 2021/10/ew-trainer/js/apps.js
@@ -3,14 +3,16 @@
 const sidebar = document.querySelector(".sidebar");
 var headerClose = document.getElementsByClassName("sidebar-close")[0];
 const mobileBtn = document.querySelector(".header-menu");
-mobileBtn.addEventListener("click", function() {
-  document.querySelector(".sidebar").classList.toggle("show");
-});
-headerClose.onclick = function() {
-  document.querySelector(".sidebar").classList.toggle("show");
+if(mobileBtn) {
+  mobileBtn.addEventListener("click", function() {
+    document.querySelector(".sidebar").classList.toggle("show");
+  });
+  headerClose.onclick = function() {
+    document.querySelector(".sidebar").classList.toggle("show");
+  }
 }
-
-var helloSlider = new Swiper(".hello-slider", {
+if(document.querySelector(".hello-slider")) {
+  var helloSlider = new Swiper(".hello-slider", {
     pagination: {
       el: ".hello-pagination",
       clickable: true,
@@ -18,27 +20,21 @@ var helloSlider = new Swiper(".hello-slider", {
     navigation: {
         nextEl: ".hello-next",
     },
-});
+  });
+  /* hide left arrow by deafult */
+  var footerBtn = document.getElementsByClassName('footer-more-btn')[0];
+  var footerLink = document.getElementsByClassName('footer-more-link')[0];
 
-/* hide left arrow by deafult */
-var footerBtn = document.getElementsByClassName('footer-more-btn')[0];
-var footerLink = document.getElementsByClassName('footer-more-link')[0];
-
-/* Swiper API - if index = 1 hide left arrow / otherwise show */
-helloSlider.on('slideChange', function() {
-  var realIndex = helloSlider.realIndex;
-
-  if (realIndex == 0) {
-    console.log("test");
-    
-  }
-  if (realIndex == 2) {
-    footerLink.style.display = "inline-block";
-    footerBtn.style.display = "none";
-  } else {
-    footerLink.style.display = "none";
-    footerBtn.style.display = "inline-block"; 
-  }
-
-});
+  /* Swiper API - if index = 1 hide left arrow / otherwise show */
+  helloSlider.on('slideChange', function() {
+    var realIndex = helloSlider.realIndex;
+    if (realIndex == 2) {
+      footerLink.style.display = "inline-block";
+      footerBtn.style.display = "none";
+    } else {
+      footerLink.style.display = "none";
+      footerBtn.style.display = "inline-block"; 
+    }
+  });
+}
 
