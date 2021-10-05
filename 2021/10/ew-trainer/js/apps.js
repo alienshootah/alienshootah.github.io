2d289c3126fef63406fc83b78de220cecdb25eb1
alienshootah <shootersmail@gmail.com>
2021-10-05 13:28:13 +0300

    ew-trainer. bug with tabs done in workout.htm done

--- 2021/10/ew-trainer/js/apps.js
+++ 2021/10/ew-trainer/js/apps.js
@@ -1,3 +1,5 @@
+
+
 /* Menu */
 
 const sidebar = document.querySelector(".sidebar");
@@ -37,4 +39,33 @@ if(document.querySelector(".hello-slider")) {
     }
   });
 }
+
+/* Tabs */
+window.addEventListener("load", function() {
+  // store tabs variable
+  var theTabs = document.querySelectorAll("ul.workout-nav > li");
+
+  function theTabClicks(tabClickEvent) {
+      var clickedTab = tabClickEvent.currentTarget;
+      var tabParent = tabClickEvent.currentTarget.parentNode.parentNode.parentNode;
+      var theTabs = tabParent.querySelectorAll("ul.workout-nav > li");
+      for (var i = 0; i < theTabs.length; i++) {
+          theTabs[i].classList.remove("active");
+      }
+      
+      clickedTab.classList.add("active");
+      tabClickEvent.preventDefault();
+      var contentPanes = tabParent.querySelectorAll(".workout-content");
+      for (i = 0; i < contentPanes.length; i++) {
+          contentPanes[i].classList.remove("active");
+      }
+      var anchorReference = tabClickEvent.target;
+      var activePaneId = anchorReference.getAttribute("href");
+      var activePane = tabParent.querySelector(activePaneId);
+      activePane.classList.add("active");
+  }
+  for (i = 0; i < theTabs.length; i++) {
+      theTabs[i].addEventListener("click", theTabClicks)
+  }
+});
 
