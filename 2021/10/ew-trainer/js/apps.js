c81bc521da08966272e39eeba2952c4ad48cf694
alienshootah <shootersmail@gmail.com>
2021-10-05 15:27:22 +0300

    pixel perfect, js on workout page. ew-trainer

--- 2021/10/ew-trainer/js/apps.js
+++ 2021/10/ew-trainer/js/apps.js
@@ -69,3 +69,91 @@ window.addEventListener("load", function() {
   }
 });
 
+
+/* Dropdowns */
+
+// Transition
+function fadeIn(el) {
+  el.style.opacity = 0;
+  el.style.display = "block";
+
+  (function fade() {
+    var val = parseFloat(el.style.opacity);
+    if (!((val += 0.1) >= 1.1)) {
+      el.style.opacity = val;
+      requestAnimationFrame(fade);
+    }
+  }());
+}
+
+function fadeOut(el) {
+  (function fade() {
+    var val = parseFloat(el.style.opacity);
+    if ((val -= 0.1) == 0) {
+      el.style.opacity = 0;
+      el.style.display = "none";
+    } else {
+      el.style.opacity = val;
+      requestAnimationFrame(fade);
+    }
+  }());
+}
+
+// Initialise all the required variables
+var btn = document.querySelectorAll(".drop-start");
+
+/*
+ *	Button click event listeners
+ *	Keeps track of the button click.
+ */
+btn.forEach(function(btn) {
+  btn.addEventListener("click", function(e) {
+    e.stopPropagation();
+    e.preventDefault();
+    var sibling = btn.nextElementSibling,
+      firstVisible = document.querySelector('.visible'),
+      dropDown;
+
+    /*
+     * Remove the visible class if an element is already in the DOM
+     */
+    if (firstVisible) {
+      fadeOut(firstVisible);
+      firstVisible.classList.remove("visible");
+    }
+
+    if (!sibling.classList.contains("visible")) {
+      fadeIn(sibling);
+      sibling.classList.add("visible");
+    } else {
+      fadeOut(sibling);
+      sibling.classList.remove("visible");
+    }
+  });
+});
+
+document.addEventListener("click", function() {
+  var visible = document.querySelector(".visible");
+
+  if (visible) {
+      fadeOut(visible);
+      visible.classList.remove("visible");
+  }
+});
+
+/* Accordion */
+
+var acc = document.getElementsByClassName("accordion-btn");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
