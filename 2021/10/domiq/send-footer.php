93fcc2529a3d6ad8bb13af104c69b7b7f34e1a98
alienshootah <shootersmail@gmail.com>
2021-10-18 09:22:02 +0300

    changes on project domiq. add sending form

---
+++ 2021/10/domiq/send-footer.php
@@ -0,0 +1,32 @@
+<?php
+$fio2 = $_POST['fio2'];
+$phone2 = $_POST['phone2'];
+$email2 = $_POST['email2'];
+$message2 = $_POST['message2'];
+
+$fio2 = htmlspecialchars($fio2);
+$phone2 = htmlspecialchars($phone2);
+$email2 = htmlspecialchars($email2);
+$message2 = htmlspecialchars($message2);
+
+$fio2 = urldecode($fio2);
+$phone2 = urldecode($phone2);
+$email2 = urldecode($email2);
+$message2 = urldecode($message2);
+
+$fio2 = trim($fio2);
+$phone2 = trim($phone2);
+$email2 = trim($email2);
+$message2 = trim($message2);
+
+// echo $fio2;
+// echo "<br>";
+// echo $phone2;
+
+if (mail("kk@2242000.ru", "Заявка с сайта", 
+"ФИО:".$fio2.". Телефон: ".$phone2. ". Email: ".$email2.". Сообщение: ".$message2,
+"From: kk@2242000.ru \r\n"))
+ {     echo "Congratulations! We have received your message and will contact you shortly.";
+} else {
+    echo "Errors occurred while sending the message. Try again";
+}?>
