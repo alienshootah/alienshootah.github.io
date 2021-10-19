1ecb988b14e391d009f91f14636769799a35681c
alienshootah <shootersmail@gmail.com>
2021-10-19 10:22:53 +0300

    adaptive 1100px ready. okna project

--- 2021/10/okna/js/apps.js
+++ 2021/10/okna/js/apps.js
@@ -87,6 +87,19 @@ if(document.querySelector(".clients-list")) {
 if(document.querySelector(".brands-list")) {
     var brandsSlider = new Swiper(".brands-list", {
         slidesPerView: 6,
+        breakpoints: {
+          640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 4,
+            spaceBetween: 40,
+          },
+          1070: {
+            slidesPerView: 5,
+          },
+        },
         loop: true,
         navigation: {
             prevEl: ".brands-arrow-left",
