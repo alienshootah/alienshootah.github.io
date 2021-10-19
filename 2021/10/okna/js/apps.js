448f6b547918be0a24c63e2f313f710bdf587d54
alienshootah <shootersmail@gmail.com>
2021-10-19 06:12:28 +0300

    js ready index page. okna project

---
+++ 2021/10/okna/js/apps.js
@@ -0,0 +1,126 @@
+/* Modals */
+
+let buttons = document.querySelectorAll('.open-modal');
+let modals = document.querySelectorAll('.modal');
+
+function showModal(id) {
+  let m = document.getElementById(id);
+  m.classList.add('visible');
+}
+
+function hideModals() {
+  modals.forEach(m => {
+    m.classList.remove('visible');
+  });
+}
+
+buttons.forEach(b => {
+  b.addEventListener('click', event => {
+    hideModals();
+    showModal(b.dataset.modal);
+  });
+});
+
+
+/**/
+modals.forEach(m => {
+  let x = m.querySelector('button.close');
+  if (x !== 'modal-body') {
+    x.addEventListener('click', hideModals);
+  }
+});
+
+/* Tabs */
+
+window.addEventListener("load", function() {
+    // store tabs variable
+    var theTabs = document.querySelectorAll("ul.types-nav > li");
+  
+    function theTabClicks(tabClickEvent) {
+        var clickedTab = tabClickEvent.currentTarget;
+        var tabParent = tabClickEvent.currentTarget.parentNode.parentNode.parentNode;
+        var theTabs = tabParent.querySelectorAll("ul.types-nav > li");
+        for (var i = 0; i < theTabs.length; i++) {
+            theTabs[i].classList.remove("active");
+        }
+        
+        clickedTab.classList.add("active");
+        tabClickEvent.preventDefault();
+        var contentPanes = tabParent.querySelectorAll(".types-content");
+        for (i = 0; i < contentPanes.length; i++) {
+            contentPanes[i].classList.remove("active");
+        }
+        var anchorReference = tabClickEvent.target;
+        var activePaneId = anchorReference.getAttribute("href");
+        var activePane = tabParent.querySelector(activePaneId);
+        activePane.classList.add("active");
+    }
+    for (i = 0; i < theTabs.length; i++) {
+        theTabs[i].addEventListener("click", theTabClicks)
+    }
+});
+
+/* Telephone mask */
+
+window.addEventListener("DOMContentLoaded", function() {
+    [].forEach.call( document.querySelectorAll('.tel'), function(input) {
+    var keyCode;
+    function mask(event) {
+        event.keyCode && (keyCode = event.keyCode);
+        var pos = this.selectionStart;
+        if (pos < 3) event.preventDefault();
+        var matrix = "+7 (___) ___ ____",
+            i = 0,
+            def = matrix.replace(/\D/g, ""),
+            val = this.value.replace(/\D/g, ""),
+            new_value = matrix.replace(/[_\d]/g, function(a) {
+                return i < val.length ? val.charAt(i++) || def.charAt(i) : a
+            });
+        i = new_value.indexOf("_");
+        if (i != -1) {
+            i < 5 && (i = 3);
+            new_value = new_value.slice(0, i)
+        }
+        var reg = matrix.substr(0, this.value.length).replace(/_+/g,
+            function(a) {
+                return "\\d{1," + a.length + "}"
+            }).replace(/[+()]/g, "\\$&");
+        reg = new RegExp("^" + reg + "$");
+        if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) this.value = new_value;
+        if (event.type == "blur" && this.value.length < 5)  this.value = ""
+    }
+  
+    input.addEventListener("input", mask, false);
+    input.addEventListener("focus", mask, false);
+    input.addEventListener("blur", mask, false);
+    input.addEventListener("keydown", mask, false)
+  
+  });
+  
+});
+
+/* Clients slider */
+
+if(document.querySelector(".clients-list")) {
+    var clientsSlider = new Swiper(".clients-list", {
+        slidesPerView: 3,
+        loop: true,
+        navigation: {
+            prevEl: ".clients-arrow-left",
+            nextEl: ".clients-arrow-right",
+        },
+    });
+}
+
+/* Brands slider */
+
+if(document.querySelector(".brands-list")) {
+    var brandsSlider = new Swiper(".brands-list", {
+        slidesPerView: 6,
+        loop: true,
+        navigation: {
+            prevEl: ".brands-arrow-left",
+            nextEl: ".brands-arrow-right",
+        },
+    });
+}
