88be9a4ed915b0a9de71f8d8e599c6f6ac713bb2
alienshootah <shootersmail@gmail.com>
2021-10-19 12:15:33 +0300

    responsive menu added js. okna project

--- 2021/10/okna/js/apps.js
+++ 2021/10/okna/js/apps.js
@@ -1,4 +1,11 @@
+/* Mobile menu */
 
+const mobileBtn = document.querySelector(".header-mobile-btn");
+if(mobileBtn) {
+  mobileBtn.addEventListener("click", function() {
+    document.querySelector(".header").classList.toggle("show");
+  });
+}
 
 /* Tabs */
 
