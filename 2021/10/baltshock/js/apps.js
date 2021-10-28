615982e6f44de615f648ffd5cf84cb10c4432f52
alienshootah <shootersmail@gmail.com>
2021-10-28 17:06:07 +0300

    index page 320px adaptive ready

--- 2021/10/baltshock/js/apps.js
+++ 2021/10/baltshock/js/apps.js
@@ -1,3 +1,12 @@
+/* Mobile menu */
+
+const mobileBtn = document.querySelector(".header-mobile-btn");
+if(mobileBtn) {
+  mobileBtn.addEventListener("click", function() {
+    document.querySelector(".header").classList.toggle("show");
+  });
+}
+
 /* Telephone mask */
 
 window.addEventListener("DOMContentLoaded", function() {
