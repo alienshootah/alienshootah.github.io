cb60316508cf363fe5e3c7b8d1753bcbeb3d3614
alienshootah <shootersmail@gmail.com>
2021-11-03 11:07:05 +0200

    login page js ready. magic project

--- 2021/10/magic/js/apps.js
+++ 2021/10/magic/js/apps.js
@@ -92,25 +92,19 @@ document.addEventListener("click", function() {
 });
 
 
-/* Show/hide pass btn 
+/* Show/hide pass btn */
 
-const mobileBtn = document.querySelector(".mobile-icon");
-if(mobileBtn) {
-  mobileBtn.addEventListener("click", function() {
-    document.querySelector(".header").classList.toggle("show");
-  });
-  window.onclick = function(event) {
-    if (!event.target.matches('.mobile-icon')) {
-      var dropdowns = document.getElementsByClassName("header");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
-      }
+const shBtn = document.querySelector(".login-text");
+shField = document.querySelector(".login-field-pass");
+if(shBtn) {
+  shBtn.addEventListener("click", function() {
+    if (shField.type === "password") {
+      shField.type = "text";
+      shBtn.textContent = "Скрыть";
+    } else {
+      shField.type = "password";
+      shBtn.textContent = "Показать";
     }
-}
+  });
 }
 
-*/
