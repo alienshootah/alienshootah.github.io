8afa1eb39956780c378314c67c2b409df7c7a2d1
alienshootah <shootersmail@gmail.com>
2021-12-18 13:14:23 +0200

    catalog page pixel perfect ready

--- 2021/12/floristika/js/apps.js
+++ 2021/12/floristika/js/apps.js
@@ -6,3 +6,22 @@ if(mobileBtn) {
     document.querySelector(".header-bottom").classList.toggle("show");
   });
 }
+
+/* Accordion */
+
+if(document.querySelector(".sidebar-btn")) {
+  var acc = document.getElementsByClassName("sidebar-btn");
+  var i;
+
+  for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var panel = this.nextElementSibling;
+      if (panel.style.display === "block") {
+      panel.style.display = "none";
+      } else {
+      panel.style.display = "block";
+      }
+  });
+  }
+}
