5f3f4151904b26d63ac428b4e8d7f642fdfef00f
alienshootah <shootersmail@gmail.com>
2022-01-12 09:53:48 +0200

    add slider to deutsch project

---
+++ 2022/deutsch/js/apps.js
@@ -0,0 +1,15 @@
+/* === SLIDER === */
+
+const reviewSlider = new Swiper('.reviews-list', {
+    slidesPerView: 2,
+    spaceBetween: 30,
+    loop: true,
+    autoHeight: true,
+    navigation: {
+      nextEl: '.reviews-arrow-right',
+      prevEl: '.reviews-arrow-left',
+    },
+    pagination: {
+        el: ".reviews-pagination",
+    },
+});
