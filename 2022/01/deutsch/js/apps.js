137f213f31b9276125968860a28571a10ff27dde
alienshootah <shootersmail@gmail.com>
2022-01-25 18:00:46 +0200

    deutch course page ready js and css

--- 2022/01/deutsch/js/apps.js
+++ 2022/01/deutsch/js/apps.js
@@ -32,3 +32,22 @@ const reviewSlider = new Swiper('.reviews-list', {
       },
     },
 });
+
+/* Accordeon */
+
+if(document.querySelector(".steps-btn")) {
+  var acc = document.getElementsByClassName("steps-btn");
+  var i;
+
+  for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var panel = this.nextElementSibling;
+      if (panel.style.display === "block") {
+      panel.style.display = "none";
+      } else {
+      panel.style.display = "block";
+      }
+  });
+  }
+}
