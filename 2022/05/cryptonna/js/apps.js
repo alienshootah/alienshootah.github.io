723e045fcb4343cd9e9bf18880d09f7bb5a32ec5
alienshootah <shootersmail@gmail.com>
2022-06-07 18:05:21 +0300

    modal

--- 2022/05/cryptonna/js/apps.js
+++ 2022/05/cryptonna/js/apps.js
@@ -139,3 +139,34 @@ window.addEventListener("load", function() {
         theTabs[i].addEventListener("click", theTabClicks)
     }
 });
+
+/* Modals */
+let buttons = document.querySelectorAll('.curr-modal');
+let modals = document.querySelectorAll('.modal');
+
+function showModal(id) {
+  let m = document.getElementById(id);
+  m.classList.add('visible');
+}
+
+function hideModals() {
+  modals.forEach(m => {
+    m.classList.remove('visible');
+  });
+}
+
+buttons.forEach(b => {
+  b.addEventListener('click', event => {
+    hideModals();
+    showModal(b.dataset.modal);
+  });
+});
+
+
+/**/
+modals.forEach(m => {
+  let x = m.querySelector('button.close');
+  if (x !== 'modal-body') {
+    x.addEventListener('click', hideModals);
+  }
+});
